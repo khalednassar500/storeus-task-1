a58845e639735fc1864468400354086234d50aaf
Khaled Nassar <khalidnassar500@gmail.com>
2024-02-09 00:53:26 +0200

    Create a home page header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import UserContainer from "./components/UserContainer";
 import "./styles/App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header className="app_header">
+        <h1>Welcome, StoreUs Team!</h1>
+      </header>
+
+      <main className="app_main">
+        <UserContainer />
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/UserContainer.tsx
@@ -0,0 +1,5 @@
+const UserContainer = () => {
+  return <section className="user_con">some text</section>;
+};
+
+export default UserContainer;
